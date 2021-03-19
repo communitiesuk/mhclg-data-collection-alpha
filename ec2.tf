9885fdbf9c0f0666b26e674b20d5c81bef79d9a5
baarkerlounger <db@slothlife.xyz>
2021-03-19 17:13:02 +0000

    Remove EC2 test

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
-locals {
-  standard_tags = {
-    application-name       = "Name of your application"
-    application-identifier = "CLDC"
-    business-unit          = "Digital Delivery"
-    budget-holder-email    = "katy.armstrong@communities.gov.uk"
-    tech-contact-email     = "mhclg@communities.gov.uk"
-    stage                  = "alpha"
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-096cb92bb3580c759"
-  instance_type = "t2.micro"
-
-  tags = local.standard_tags
-}
+# locals {
+#   standard_tags = {
+#     application-name       = "Name of your application"
+#     application-identifier = "CLDC"
+#     business-unit          = "Digital Delivery"
+#     budget-holder-email    = "katy.armstrong@communities.gov.uk"
+#     tech-contact-email     = "mhclg@communities.gov.uk"
+#     stage                  = "alpha"
+#   }
+# }
+#
+# resource "aws_instance" "example" {
+#   ami           = "ami-096cb92bb3580c759"
+#   instance_type = "t2.micro"
+#
+#   tags = local.standard_tags
+# }
