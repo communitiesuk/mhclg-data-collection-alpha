e4681448a4213d5dafec954ff313ff6c74b87c08
baarkerlounger <db@slothlife.xyz>
2021-03-24 10:35:19 +0000

    Add SSE to S3 bucket

--- s3.tf
+++ s3.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "core-data" {
   bucket = "mhclg-core-data-upload"
   acl    = "private"
+
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = var.default_tags
 }
