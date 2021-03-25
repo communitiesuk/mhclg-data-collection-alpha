f3b78ac747bb078d6c13e6489f712df6cfd58648
baarkerlounger <db@slothlife.xyz>
2021-03-25 10:39:01 +0000

    Block all public access to S3 bucket until required

--- s3.tf
+++ s3.tf
@@ -16,3 +16,12 @@ resource "aws_s3_bucket" "core-data" {
 
   tags = var.default_tags
 }
+
+resource "aws_s3_bucket_public_access_block" "core-data-public-access-block" {
+  bucket = aws_s3_bucket.core-data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
