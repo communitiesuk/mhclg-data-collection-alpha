cebcb8646562fb0d744b813759c726f20fe08542
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-04-08 10:30:41 +0100

    Send uploaded file to tabular parser and render input template
    Co-Authored-By: Matty Phelan <matthew.phelan@madetech.com>

--- modules/prototype-kit/codebase/app/routes.js
+++ modules/prototype-kit/codebase/app/routes.js
@@ -1,6 +1,24 @@
+const axios = require('axios')
 const express = require('express')
+const multer = require('multer')
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post(
+  '/upload/tabular-data',
+  upload.single('file'),
+  async (req, res) => {
+    const url = process.env.TABULAR_PARSER_URI
+    const {buffer: file, mimetype} = req.file
+    const {data: parsedTabularDocument} = await axios.post(url, file)
+
+    res.render(req.query.template || "mapping-2", {
+      parsedTabularDocument,
+    })
+  },
+)
 
 module.exports = router
