61b4ce1454c3459ce086056986bfbdd545ea75fd
baarkerlounger <db@slothlife.xyz>
2021-04-08 12:10:18 +0100

    Typo and Pytest

--- modules/lambda-tabular-parser/test_main.py
+++ modules/lambda-tabular-parser/test_main.py
@@ -6,6 +6,6 @@ from main import *
 class TestExcelParser:
     def test_header_parsing(self):
         fixture_path = os.getcwd() + '/fixtures/Capita.xlsx'
-        result = json.loads(parse_excel(fixture_path))["columns"]
+        result = parse_tabular(fixture_path)["columns"]
         assert result[5] == "First letting?"
         assert result[24] == "Gender of Person 6"
