e8d030318c7e9a4adf9243e7ee419f8761adbdc7
Matthew Phelan <matthew.phelan@madetech.com>
2021-04-09 14:58:00 +0100

    start blurb page added

--- modules/prototype-kit/codebase/app/config.js
+++ modules/prototype-kit/codebase/app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'MHCLG Data Collection Alpha',
+  serviceName: 'Share data with MHCLG',
 
   // Default port that prototype runs on
   port: '3000',
