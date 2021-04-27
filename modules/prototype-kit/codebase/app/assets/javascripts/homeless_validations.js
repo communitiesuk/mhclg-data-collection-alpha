3ddde6adc31d88fb656b586040234dbcf0fa1329
Anthony Roy <anthony.roy@madetech.com>
2021-04-27 12:03:17 +0100

    Homeless now in 6 fields

--- modules/prototype-kit/codebase/app/assets/javascripts/homeless_validations.js
+++ modules/prototype-kit/codebase/app/assets/javascripts/homeless_validations.js
@@ -2,6 +2,7 @@ const homeless_validations = [
 ]
 
 const homeless_options = [
+    { label: "Not Homeless", value: "not_homeless" },
     { label: "Homeless or about to lose their home (within 56  days)", value: "homeless" },
     { label: "Living in insanitary, overcrowded or unsatisfactory housing", value: "unsatisfactory_housing" },
     { label: "A need to move on medical and welfare grounds (including a disability)", value: "welfare" },
@@ -9,44 +10,41 @@ const homeless_options = [
     { label: "Don't know", value: "unknown" },
 ]
 
-const homeless1 = {
-    label: "Homeless 1", key: "homeless1",
-    validators: homeless_validations,
+const homelessCombined = {
+    label: "Homeless Status in Words (only if the other homeless columns cannot be mapped)", key: "homeless_status",
     type: 'select',
     options: homeless_options,
     matchStrategy: 'fuzzy'
+}
+
+const homeless1 = {
+    label: "Homeless or about to lose home", key: "homeless",
+    alternates: ["Homeless or about to lose their home (within 56 days)"],
+    type: 'checkbox',
 }
 
 const homeless2 = {
-    label: "Homeless 2", key: "homeless2",
-    validators: homeless_validations,
-    type: 'select',
-    options: homeless_options,
-    matchStrategy: 'fuzzy'
+    label: "Insanitary or overcrowded housing", key: "unsatisfactory_housing",
+    alternates: ["Living in insanitary, overcrowded or unsatisfactory housing"],
+    type: 'checkbox',
 }
 
 const homeless3 = {
-    label: "Homeless 3", key: "homeless3",
-    validators: homeless_validations,
-    type: 'select',
-    options: homeless_options,
-    matchStrategy: 'fuzzy'
+    label: "Move due to medical or welfare", key: "welfare",
+    alternates: ["A need to move on medical and welfare grounds (including a disability)"],
+    type: 'checkbox',
 }
 
 const homeless4 = {
-    label: "Homeless 4", key: "homeless4",
-    validators: homeless_validations,
-    type: 'select',
-    options: homeless_options,
-    matchStrategy: 'fuzzy'
+    label: "Move to avoid hardship", key: "hardship",
+    alternates: ["A need to move to avoid hardship to themselves or others"],
+    type: 'checkbox',
 }
 
 const homeless5 = {
-    label: "Homeless 5", key: "homeless5",
-    validators: homeless_validations,
-    type: 'select',
-    options: homeless_options,
-    matchStrategy: 'fuzzy'
+    label: "Unknown", key: "unknown",
+    alternates: ["Don't know"],
+    type: 'checkbox',
 }
 
 function mapHomelessness(entry){
@@ -56,25 +54,20 @@ function mapHomelessness(entry){
     entry.hardship = false;
     entry.unknown = false;
 
-    [entry.homeless1, entry.homeless2, entry.homeless3, entry.homeless4, entry.homeless5].map(
-        (homelessField) => {
-            if (homelessField) {
-                switch (homelessField) {
-                    case 'homeless':
-                        entry.homeless = true; break;
-                    case 'unsatisfactory_housing':
-                        entry.unsatisfactory_housing = true; break;
-                    case 'welfare':
-                        entry.welfare = true; break;
-                    case 'hardship':
-                        entry.hardship = true; break;
-                    case 'unknown':
-                        entry.unknown = true; break;
-                }
-            }
+    if (entry.homeless_status) {
+        switch (entry.homeless_status){
+            case 'homeless':
+                entry.homeless = true; break;
+            case 'unsatisfactory_housing':
+                entry.unsatisfactory_housing = true; break;
+            case 'welfare':
+                entry.welfare = true; break;
+            case 'hardship':
+                entry.hardship = true; break;
+            case 'unknown':
+                entry.unknown = true; break;
         }
-    )
-    if (!entry.homeless && !entry.unsatisfactory_housing && !entry.welfare && !entry.hardship) entry.unknown = true;
+    }
 
     console.log(`Homeless: ${entry.homeless}; Unsatisfactory: ${entry.unsatisfactory_housing}; Welfare: ${entry.welfare}; Hardship: ${entry.hardship}; Unknown: ${entry.unknown}`);
     return entry;
