811e09385dae93472aac2f1ea857e8fdd8024c0a
Anthony Roy <anthony.roy@madetech.com>
2021-05-10 17:52:11 +0100

    Added bad test data csv. Added questionnaire.py

---
+++ modules/form-generation/questionnaire.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime as dt
+import sys
+import yaml
+
+
+def red(text):
+    return "\u001b[31;1m%s\u001b[0m" % text
+
+
+def yellow(text):
+    return "\u001b[33;1m%s\u001b[0m" % text
+
+
+def run_section(section):
+    print()
+    print(yellow(section['label']))
+
+    for question in section['questions']:
+        answer = ask(question)
+
+
+def validate(question, answer):
+    if question['mandatory'] and not answer:
+        print(red("This question is mandatory"))
+        return False
+    if question['question_type'] == "multiple_choice_select":
+        options = [opt['label'] for opt in question["answer_options"]]
+        if answer not in options:
+            print(red("Answer must be one of: %s" % "; ".join(options)))
+            return False
+    if question['question_type'] == "date":
+        try:
+            dt.strptime(answer, "%d/%m/%Y")
+        except:
+            print(red("Invalid date format. Use: DD/MM/YYYY"))
+            return False
+    return True
+
+def ask(question):
+    answer = input("%s " % question["label"])
+    if validate(question, answer):
+        return answer
+
+    ask(question)
+
+
+
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+
+    with open(filename) as fh:
+        data = yaml.load(fh, Loader=yaml.SafeLoader)
+
+    for section in data['sections']:
+        run_section(section)
+
+
