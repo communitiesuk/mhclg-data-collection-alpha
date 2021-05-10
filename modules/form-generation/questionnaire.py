8f4794b5a9ba3577ae61b158ba766bcf59b96312
Anthony Roy <anthony.roy@madetech.com>
2021-05-10 17:57:48 +0100

    Tidy up validators

--- modules/form-generation/questionnaire.py
+++ modules/form-generation/questionnaire.py
@@ -22,22 +22,37 @@ def run_section(section):
         answer = ask(question)
 
 
+def validate_select(question, answer):
+    options = [opt['label'] for opt in question["answer_options"]]
+
+    if answer not in options:
+        print(red("Answer must be one of: %s" % "; ".join(options)))
+        return False
+
+    return True
+
+
+def validate_date(question, answer):
+    try:
+        dt.strptime(answer, "%d/%m/%Y")
+    except:
+        print(red("Invalid date format. Use: DD/MM/YYYY"))
+        return False
+
+    return True
+
+validators = {
+    'multiple_choice_select': validate_select,
+    'date': validate_date
+}
+
+
 def validate(question, answer):
     if question['mandatory'] and not answer:
         print(red("This question is mandatory"))
         return False
-    if question['question_type'] == "multiple_choice_select":
-        options = [opt['label'] for opt in question["answer_options"]]
-        if answer not in options:
-            print(red("Answer must be one of: %s" % "; ".join(options)))
-            return False
-    if question['question_type'] == "date":
-        try:
-            dt.strptime(answer, "%d/%m/%Y")
-        except:
-            print(red("Invalid date format. Use: DD/MM/YYYY"))
-            return False
-    return True
+
+    return validators[question['question_type']](question, answer)
 
 def ask(question):
     answer = input("%s " % question["label"])
