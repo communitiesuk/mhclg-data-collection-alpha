06fc590048609384c3e4fda310fd5a91a6f6e907
baarkerlounger <db@slothlife.xyz>
2021-05-12 16:31:54 +0100

    Use py files

--- modules/pandas/tests/test_suite.py
+++
@@ -1,9 +0,0 @@
-import os
-import papermill as pm
-
-notebook_path = os.path.join(os.path.dirname(__file__), 'weighting.ipynb')
-
-pm.execute_notebook(
-    notebook_path,
-    'output.ipynb'
-)
--- modules/pandas/tests/test_weighting.py
+++ modules/pandas/tests/test_weighting.py
@@ -1,31 +1,29 @@
----
-jupyter:
-  jupytext:
-    formats: ipynb,md
-    text_representation:
-      extension: .md
-      format_name: markdown
-      format_version: '1.3'
-      jupytext_version: 1.11.2
-  kernelspec:
-    display_name: Python 3
-    language: python
-    name: python3
----
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
-```python
+# +
 # Setup ipytest extension
 
 import ipytest
 ipytest.autoconfig()
-```
+# -
 
-```python
 # Parameters
 base_file_path = %pwd
-```
 
-```python
+# +
 # Import our dependencies
 
 import os
@@ -34,9 +32,8 @@ from pathlib import Path
 import pytest
 import pandas
 from pandas.testing import assert_frame_equal
-```
 
-```python
+# +
 # Import our data model
 
 module_path = os.path.abspath(os.path.join('../'))
@@ -44,43 +41,36 @@ if module_path not in sys.path:
     sys.path.append(module_path)
     
 from weighting.weighting import apply_weighting
-```
+# -
 
-```python
 path = os.path.normpath(os.path.join(Path().absolute(), '../weighting/fixtures'))
-path
-```
 
-```python
+# +
 # Set up our input data from a CSV fixture
 
 input_data = pandas.read_csv(path + '/input.csv', index_col=0)
 input_data
-```
 
-```python
+# +
 # Set up our expected result from a CSV fixture
 
 expected_result = pandas.read_csv(path + '/expected_result.csv', index_col=0)
 expected_result
-```
 
-```python
+# +
 # Run the model
 
 actual_result = apply_weighting(input_data)
 actual_result
-```
 
-```python
-%%run_pytest[clean]
+
+# +
+# %%run_pytest[clean]
 
 # Check that our actual result matches our expected result
 
 def test_apply_weighting():
     assert_frame_equal(actual_result, expected_result)
-```
+# -
 
-```python
 
-```
