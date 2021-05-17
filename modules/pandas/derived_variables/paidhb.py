f51b1c4a5e2a74adb662695b845c5348bfedc829
Anthony Roy <anthony.roy@madetech.com>
2021-05-17 12:21:07 +0100

    Refactored personal allowance

--- modules/pandas/derived_variables/paidhb.py
+++ modules/pandas/derived_variables/paidhb.py
@@ -112,41 +112,49 @@ def couple_and_both_are_under_18(dataframe, tenant_number):
 
     return lead_tenant_under_18 & this_tenant_under_18 & this_tenant_is_partner
 
+
+def couple_and_one_is_over_18(dataframe, tenant_number):
+    lead_tenant_over_18 = dataframe["AGE1"] >= 18
+    this_tenant_over_18 = dataframe["AGE%s" % tenant_number] >= 18 
+    this_tenant_is_partner = dataframe["RELAT2"] == "P"
+
+    return (lead_tenant_over_18 | this_tenant_over_18) & this_tenant_is_partner
+
+
+def set_personal_allowance(dataframe, filter, allowance):
+    dataframe.loc[filter, "personal_allowance"] = allowance
+
+
 def personal_allowance(dataframe):
     dataframe["personal_allowance"] = 0
 
     single_adult_under_25 = (dataframe["AGE1"] < 25) & (dataframe["TOTADULT"] == 1) & (dataframe["TOTCHILD"] == 0)
-    dataframe.loc[single_adult_under_25, ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE1
-
     single_parent_age_16_17 = (dataframe["AGE1"] >= 16) & (dataframe["AGE1"] <= 17) & (dataframe["TOTCHILD"] > 0)
-    dataframe.loc[single_parent_age_16_17, ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE1
+
+    set_personal_allowance(dataframe, single_adult_under_25 | single_parent_age_16_17, PERSONAL_ALLOWANCE_TYPE1)
 
     single_adult_over_25 = (dataframe["AGE1"] >= 25) & (dataframe["TOTADULT"] == 1) & (dataframe["TOTCHILD"] == 0)
-    dataframe.loc[single_adult_over_25, ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE2
-
     single_parent_over_18 = (dataframe["AGE1"] >= 18) & (dataframe["TOTCHILD"] > 0)
-    dataframe.loc[single_parent_over_18, ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE2
 
-    # Couple, both under 18
-    dataframe.loc[ \
-        couple_and_both_are_under_18(dataframe, 2) | \
-        couple_and_both_are_under_18(dataframe, 3) | \
-        couple_and_both_are_under_18(dataframe, 4) | \
-        couple_and_both_are_under_18(dataframe, 5) | \
-        couple_and_both_are_under_18(dataframe, 6) | \
-        couple_and_both_are_under_18(dataframe, 7) | \
-        couple_and_both_are_under_18(dataframe, 8), ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE3
+    set_personal_allowance(dataframe, single_adult_over_25 | single_parent_over_18, PERSONAL_ALLOWANCE_TYPE2)
 
-    # Couple, at least one over 18
-    dataframe.loc[ \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE2"] > 18)) & (dataframe["RELAT2"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE3"] > 18)) & (dataframe["RELAT3"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE4"] > 18)) & (dataframe["RELAT4"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE5"] > 18)) & (dataframe["RELAT5"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE6"] > 18)) & (dataframe["RELAT6"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE7"] > 18)) & (dataframe["RELAT7"] == "P")) | \
-        (((dataframe["AGE1"] > 18) | (dataframe["AGE8"] > 18)) & (dataframe["RELAT8"] == "P")), ["personal_allowance"]] = PERSONAL_ALLOWANCE_TYPE4
+    possible_young_couples = [couple_and_both_are_under_18(dataframe, tenant_no) for tenant_no in range(2, 9)]
 
+    young_couple = possible_young_couples[0]
+
+    for possible_couple in possible_young_couples[1:]:
+        young_couple = young_couple | possible_couple
+
+    set_personal_allowance(dataframe, young_couple, PERSONAL_ALLOWANCE_TYPE3)
+
+    possible_couples = [couple_and_one_is_over_18(dataframe, tenant_no) for tenant_no in range(2, 9)]
+
+    couple = possible_couples[0]
+
+    for possible_couple in possible_couples[1:]:
+        couple = couple | possible_couple
+
+    set_personal_allowance(dataframe, couple, PERSONAL_ALLOWANCE_TYPE4)
     return dataframe["personal_allowance"]
 
 
