db680f657e58b0f0d0d0c5c5eca65384f3e313e5
baarkerlounger <db@slothlife.xyz>
2021-08-16 14:18:16 +0100

    Pandas script to extract device types and browsers from server logs

---
+++ modules/logs-analysis/parser.py
@@ -0,0 +1,39 @@
+import pandas
+import sys
+from user_agents import parse
+
+file = sys.argv[1]
+logs = pandas.read_csv(file)
+ua_column = 'Connection from what OS/Software'
+user_agents = logs[ua_column].value_counts()
+user_agents = user_agents.to_frame(name='count').reset_index().rename(columns = {'index':'user-agent'})
+
+oses = []
+browsers = []
+parsed_user_agents = []
+devices = []
+for ua_string in user_agents['user-agent']:
+    user_agent = parse(ua_string)
+    parsed_user_agents.append(str(user_agent))
+    oses.append(user_agent.os.family + " " + user_agent.os.version_string)
+    browsers.append(user_agent.browser.family)
+    if user_agent.is_pc:
+        device = 'PC'
+    elif user_agent.is_mobile:
+        device = 'Mobile'
+    elif user_agent.is_tablet:
+        device = 'Tablet'
+    else:
+        device = 'Other'
+    devices.append(device)
+
+user_agents['parsed'] = pandas.Series(parsed_user_agents)
+user_agents['device'] = pandas.Series(devices)
+user_agents['os'] = pandas.Series(oses)
+user_agents['browser'] = pandas.Series(browsers)
+
+user_agents.drop(user_agents[user_agents['browser'] == 'Googlebot'].index, inplace=True)
+user_agents.drop(user_agents[user_agents['browser'] == 'DuckDuckGo-Favicons-Bot'].index, inplace=True)
+
+grouped = user_agents.groupby(['device', 'os', 'browser'])['count'].sum().sort_values(ascending=False)
+print(grouped)
