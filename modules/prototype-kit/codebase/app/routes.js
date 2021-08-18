6e0f20e30cdf6416d1cbf5ff3f3509299d71a0a8
baarkerlounger <db@slothlife.xyz>
2021-08-18 12:55:23 +0100

    Routing

--- modules/prototype-kit/codebase/app/routes.js
+++ modules/prototype-kit/codebase/app/routes.js
@@ -60,123 +60,74 @@ router.get('/sprint10/nextIncompleteSection', function (req, res) {
   res.redirect(incompleteSections[0] + '-section')
 })
 
-router.get('/sprint10/household-section', function (req, res) {
-
-  const section = 'household/'
-  const sectionPages = ['tenant-code', 'tenant-age', 'gender', 'ethnicity', 'nationality', 'economic-status', 'other-household-members']
+function redirectToNextFormPage(req, res, section, sectionPages) {
+  const referer = req.headers.referer
   const incompleteSectionPages = []
   sectionPages.forEach(function (page, i) {
     if(!req.session.data[page + '-page']) {
       incompleteSectionPages.push(page)
     }
   });
+  let next = ''
+  if(req.session.data.onlyMissing){
+    if(incompleteSectionPages.length > 1){
+      next = '?next=' + incompleteSectionPages[1]
+    } else {
+      next = '?next=check-answers'
+    }
+  } else if (referer.endsWith("check-answers")) {
+    next = '?next=check-answers'
+  }
+  let redirect = section + incompleteSectionPages[0] + next
+  res.redirect(redirect)
+}
 
-  res.redirect(section + incompleteSectionPages[0])
+router.get('/sprint10/household-section', function (req, res) {
+  const section = 'household/'
+  const sectionPages = ['tenant-code', 'tenant-age', 'gender', 'ethnicity', 'nationality', 'economic-status', 'other-household-members']
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/household-situation-section', function (req, res) {
-
   const section = 'household-situation/'
   const sectionPages = ['previous-housing-situation', 'homeless', 'reason-for-leaving']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
-})
-
-router.get('/sprint10/household-needs-section', function (req, res) {
-
-  const section = 'household-needs/'
-  const sectionPages = ['armed-forces', 'disabilities', 'pregnancy', 'requirements', 'conditions']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/tenancy-section', function (req, res) {
-
   const section = 'tenancy/'
   const sectionPages = ['tenancy-code', 'tenancy-start-date', 'starter-tenancy', 'fixed-term', 'type-of-main-tenancy', 'letting-type', 'landlord']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/property-section', function (req, res) {
-
   const section = 'property/'
   const sectionPages = ['property-location', 'postcode', 'relet', 'reason-for-vacancy', 'property-reference',
   'type-of-unit', 'type-of-building', 'number-of-bedrooms', 'void-date', 'repair-date', 'previously-offered', 'wheelchair-accessible']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/income-and-benefits-section', function (req, res) {
-
   const section = 'income-and-benefits/'
   const sectionPages = ['income', 'benefit-proportion', 'benefits']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/rent-section', function (req, res) {
-
   const section = 'rent/'
   const sectionPages = ['rent']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/local-authority-section', function (req, res) {
-
   const section = 'local-authority/'
   const sectionPages = ['time-lived-in-la', 'time-on-waiting-list', 'previous-la', 'previous-postcode', 'reasonable-preference', 'allocation-type']
-  const incompleteSectionPages = []
-  sectionPages.forEach(function (page, i) {
-    if(!req.session.data[page + '-page']) {
-      incompleteSectionPages.push(page)
-    }
-  });
-
-  res.redirect(section + incompleteSectionPages[0])
+  redirectToNextFormPage(req, res, section, sectionPages)
 })
 
 router.get('/sprint10/submission-section', function (req, res) {
   const section = 'submission/'
   res.redirect(section + 'declaration')
 })
-
 
 module.exports = router
