edbb4fdbddbb04eb9712a63a6aa6cc9affc00031
baarkerlounger <db@slothlife.xyz>
2021-08-24 15:14:46 +0100

    Update log script

--- modules/logs-analysis/parser.py
+++ modules/logs-analysis/parser.py
@@ -1,45 +1,69 @@
+import os
+import glob
 import pandas
 import sys
 from user_agents import parse
 
-file = sys.argv[1]
-logs = pandas.read_csv(file)
-ua_column = 'Connection from what OS/Software'
-user_agents = logs[ua_column].value_counts()
-user_agents = user_agents.to_frame(name='count').reset_index().rename(columns = {'index':'user-agent'})
+colnames = ['Date Time', 'Time Zone', 'Connection From', 'col3', 'col4', 'Action',
+            'Size', 'Request', 'Response', 'URL Requested', 'Connection from what OS/Software',
+            'Public IP address', 'Cloudfront ID', 'rec']
 
-oses = []
-browsers = []
-parsed_user_agents = []
-devices = []
-for ua_string in user_agents['user-agent']:
-    user_agent = parse(ua_string)
-    parsed_user_agents.append(str(user_agent))
-    oses.append(user_agent.os.family + " " + user_agent.os.version_string)
-    browsers.append(user_agent.browser.family)
-    if user_agent.is_pc:
-        device = 'PC'
-    elif user_agent.is_mobile:
-        device = 'Mobile'
-    elif user_agent.is_tablet:
-        device = 'Tablet'
-    else:
-        device = 'Other'
-    devices.append(device)
+dir = sys.argv[1]
+os.chdir(dir)
 
-user_agents['parsed'] = pandas.Series(parsed_user_agents)
-user_agents['device'] = pandas.Series(devices)
-user_agents['os'] = pandas.Series(oses)
-user_agents['browser'] = pandas.Series(browsers)
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+for file in all_filenames:
+    logs = pandas.read_csv(file, names=colnames, header=None)
+    ua_column = 'Connection from what OS/Software'
+    user_agents = logs[ua_column].value_counts()
+    user_agents = user_agents.to_frame(name='count').reset_index().rename(columns = {'index':'user-agent'})
 
-user_agents.drop(user_agents[user_agents['browser'] == 'Googlebot'].index, inplace=True)
-user_agents.drop(user_agents[user_agents['browser'] == 'DuckDuckGo-Favicons-Bot'].index, inplace=True)
+    oses = []
+    browsers = []
+    parsed_user_agents = []
+    devices = []
+    for ua_string in user_agents['user-agent']:
+        user_agent = parse(ua_string)
+        parsed_user_agents.append(str(user_agent))
+        oses.append(user_agent.os.family + " " + user_agent.os.version_string)
+        browsers.append(user_agent.browser.family)
+        if user_agent.is_pc:
+            device = 'PC'
+        elif user_agent.is_mobile:
+            device = 'Mobile'
+        elif user_agent.is_tablet:
+            device = 'Tablet'
+        else:
+            device = 'Other'
+        devices.append(device)
+
+    user_agents['parsed'] = pandas.Series(parsed_user_agents)
+    user_agents['device'] = pandas.Series(devices)
+    user_agents['os'] = pandas.Series(oses)
+    user_agents['browser'] = pandas.Series(browsers)
+
+    user_agents.to_csv('../combined.csv', mode='a', header=False)
+
+file = '../combined.csv'
+colnames = ['idx', 'user-agent', 'count', 'parsed', 'device', 'os', 'browser', 'rec']
+user_agents = pandas.read_csv(file, names=colnames, header=None, index_col=0)
+
+excluded_agents = ['Googlebot', 'DuckDuckGo-Favicons-Bot', 'Magus Bot', 'bingbot',
+                    'HeadlessChrome', 'DotBot', 'Nimbostratus-Bot', 'SEMrushBot',
+                    'GooglePlusBot', 'SiteAuditBot', 'MojeekBot', 'BaiduSpider',
+                    'SiteCheck-sitecrawl', 'DataForSeoBot', 'serpstatbot', 'oBot',
+                    'crawler', 'webprosbot', 'Amazonbot', 'digitalshadowsbot',
+                    'MJ12bot', 'SemrushBot', 'MauiBot', 'WebPageTest.org bot', 'Slackbot',
+                    'spider', 'Applebot', 'Googlebot-Image', 'okhttp', 'BingPreview',
+                    'Slack-ImgProxy', 'FacebookBot', 'Mediapartners-Google',
+                    'Python Requests', 'Python-urllib', 'Slackbot-LinkExpanding', 'BLEXBot']
+user_agents.drop(user_agents[user_agents['browser'].isin(excluded_agents)].index, inplace=True)
+user_agents.drop(user_agents[(user_agents['browser'] == 'Other') & (user_agents['os'] == 'Other ') & (user_agents['device'] == 'Other')].index, inplace=True)
 
 grouped = user_agents.groupby(['device', 'os', 'browser'])['count'].sum().sort_values(ascending=False)
-print(grouped)
-
-import psutil
-memory = psutil.virtual_memory()
-print(f" {'*' * 3} Memory used percentage - {memory.percent} \n {'*' * 4} Free Memory available - { round(memory.free / (1024.0 ** 3))} GB")
-
-print(f" ** Memory usage of the file - {sum(logs.memory_usage()) * 0.000001} MB for {len(logs.index)} Rows")
+total = grouped.sum()
+percentage = grouped / total * 100
+percentage.to_csv('../percentage.csv')
+with pandas.option_context('display.max_rows', None):
+    print(percentage)
