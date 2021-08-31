4b8564c8f013aa323bd3a85566d069df51cf615a
roobottom <roo@roodesign.co.uk>
2021-08-31 10:45:11 +0100

    updated service name

--- modules/prototype-kit/codebase/app/config.js
+++ modules/prototype-kit/codebase/app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Submit data on social housing sales and lettings',
+  serviceName: 'Submit CORE logs for social housing sales and lettings',
 
   // Default port that prototype runs on
   port: '3000',
